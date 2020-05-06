689a901af93ac7cfed662afea88a254494f69d69
seunghyun kim <tmdgusdl92@gmail.com>
2020-05-07 05:41:10 +0900

    Updates

---
+++ precache-manifest.8cec15fda94c7cdb46b477208aa0aa89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e454600d7d948e46e370dc4a0ef526",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a378fbac2c1df7480243",
+    "url": "/movie_app_2020/static/css/main.05d37451.chunk.css"
+  },
+  {
+    "revision": "261fc6083f1d5d248b8b",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.769f61a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a378fbac2c1df7480243",
+    "url": "/movie_app_2020/static/js/main.0a74ff17.chunk.js"
+  },
+  {
+    "revision": "9927c9d5c16bc5ac0d63",
+    "url": "/movie_app_2020/static/js/runtime-main.72517e8e.js"
+  }
+]);
